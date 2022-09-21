0ba48ea9bd3f2f01e14945f96f4941dfc3da1371
jsevilla202 <jsevilla@iesnervion.es>
2022-09-21 13:34:54 +0200

    EjBasico5

--- EjBasico5/src/ejbasico5/EjBasico5.java
+++ EjBasico5/src/ejbasico5/EjBasico5.java
@@ -1,9 +1,17 @@
 package ejbasico5;
  import java.lang.Math;
+import java.util.Scanner;
 public class EjBasico5 {
 
 	public static void main(String[] args) {
-		System.out.println(Math.PI);
+		System.out.println("Introduzca el radio de la circunferencia: ");
+		Scanner sc = new Scanner(System.in);
+		double radio = sc.nextDouble();
+		double longitud = 2*Math.PI*radio;
+		double area = Math.PI*(radio*radio);
+		System.out.println("La longitud es: " + longitud);
+		System.out.println("El area es: " + area);
+		
 
 	}
 
