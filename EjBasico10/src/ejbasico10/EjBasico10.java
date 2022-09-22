957cf2929aad272fc90eeed09d5e8f8c154e210c
jsevilla202 <jsevilla@iesnervion.es>
2022-09-22 13:37:14 +0200

    Comentado EjBasico10

--- EjBasico10/src/ejbasico10/EjBasico10.java
+++ EjBasico10/src/ejbasico10/EjBasico10.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class EjBasico10 {
 
 	public static void main(String[] args) {
+		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		//Damos valor a la variable IVA
 		double IVA = 0.21;
+		//Pedimos al usuario que inserte el precio del producto
 		System.out.print("Indique el precio del producto: ");
+		//Usamos el scanner para dar valor a la variable "precio"
 		double precio = sc.nextDouble();
+		//Damos valor a la variable "preciot" (precio total)
 		double preciot = (precio*IVA)+precio;
+		//Mostramos por consola el resultado
 		System.out.println("El precio del producto con el IVA incluido es de "+preciot + " euros");
 		
 	}
