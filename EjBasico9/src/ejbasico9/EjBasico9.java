aff0c2f263b4d0140f768cb38b95a3ee7b245c29
jsevilla202 <jsevilla@iesnervion.es>
2022-09-22 13:14:46 +0200

    Comentado Ejbasico9

--- EjBasico9/src/ejbasico9/EjBasico9.java
+++ EjBasico9/src/ejbasico9/EjBasico9.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class EjBasico9 {
 
 	public static void main(String[] args) {
+		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		//Pedimos al usuario que introduzca la cantidad de pesetas
 		System.out.print("Introduzca la cantidad de pesetas: ");
+		//Buscamos el numero de pesetas introducido por el usuario
 		int pesetas = sc.nextInt();
+		//Creamos la variable euros y le damos el valor de la ecuación
 		double euros = pesetas*0.006;
+		//Mostramos la conversión por consola
 		System.out.println("Tienes un total de: " + euros + " euros");
 
 	}
