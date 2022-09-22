4743e5c4994c6796700301e17c629abd1fa2d37f
jsevilla202 <jsevilla@iesnervion.es>
2022-09-22 13:26:20 +0200

    Comentado EjBasico5

--- EjBasico5/src/ejbasico5/EjBasico5.java
+++ EjBasico5/src/ejbasico5/EjBasico5.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 public class EjBasico5 {
 
 	public static void main(String[] args) {
-		System.out.println("Introduzca el radio de la circunferencia: ");
+		//Pedimos al usuario que inserte el radio de la circunferencia
+		System.out.print("Introduzca el radio de la circunferencia: ");
+		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		//Usamos el scanner para dar valor a la variable "radio"
 		double radio = sc.nextDouble();
+		//Damos valor a la variable "longitud" y "area"
 		double longitud = 2*Math.PI*radio;
 		double area = Math.PI*(radio*radio);
+		//Mostramos por consola los resultados
 		System.out.println("La longitud es: " + longitud);
 		System.out.println("El area es: " + area);
 		
