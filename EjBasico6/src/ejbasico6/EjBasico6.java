2b2cb9a78fcd6b8ad3b3dbab6326e40726ef5d51
jsevilla202 <jsevilla@iesnervion.es>
2022-09-22 13:23:49 +0200

    Comentado EjBasico6

--- EjBasico6/src/ejbasico6/EjBasico6.java
+++ EjBasico6/src/ejbasico6/EjBasico6.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 public class EjBasico6 {
 
 	public static void main(String[] args) {
+		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Intruduzca un numero: ");
+		//Pedimos al usuario que inserte un numero
+		System.out.print("Intruduzca un numero: ");
+		//Usamos el scanner para dar valor a la variable "numero1"
 		double numero1 = sc.nextDouble();
-		System.out.println("Intruduzca otro numero: ");
+		//Pedimos al usuario que inserte otro numero
+		System.out.print("Intruduzca otro numero: ");
+		//Usamos el scanner para dar valor a la variable "numero2"
 		double numero2 = sc.nextDouble();
+		//Damos valores a las variables "suma", "resta", "multi" y "divi"
 		double suma = numero1 + numero2;
 		double resta = numero1 - numero2;
 		double multi = numero1 * numero2;
 		double divi = numero1 / numero2;
+		//Mostramos por consola los resultado
 		System.out.println("La suma de los numeros es: "+suma);
 		System.out.println("La resta de los numeros es: "+resta);
 		System.out.println("La multiplicacion de los numeros es: "+multi);
