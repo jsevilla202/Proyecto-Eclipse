67ee79100d14c36d71c27efe92ccf5f6b4c98927
jsevilla202 <jsevilla@iesnervion.es>
2022-09-22 13:21:20 +0200

    Comentado EjBasico7

--- EjBasico7/src/ejbasico7/EjBasico7.java
+++ EjBasico7/src/ejbasico7/EjBasico7.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class EjBasico7 {
 
 	public static void main(String[] args) {
+		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Inserte su nombre: ");
+		//Pedimos al usuario que inserte su nombre
+		System.out.print("Inserte su nombre: ");
+		//Usamos el scanner para dar valor a la variable "nombre"
 		String nombre = sc.nextLine();
-		System.out.println("Intruduzca su calle: ");
+		//Pedimos al usuario que inserte su calle
+		System.out.print("Intruduzca su calle: ");
+		//Usamos el scanner para dar valor a la variable "calle"
 		String calle = sc.nextLine();
-		System.out.println("Intruduzca su numero: ");
+		//Pedimos al usuario que inserte su numero
+		System.out.print("Intruduzca su numero: ");
+		//Usamos el scanner para dar valor a la variable "numero"
 		int numero = sc.nextInt();
+		//Mostramos por consola el resultado
 		System.out.println("Nombre: " + nombre);
 		System.out.println("Calle: " + calle);
 		System.out.println("Numero: " + numero);
