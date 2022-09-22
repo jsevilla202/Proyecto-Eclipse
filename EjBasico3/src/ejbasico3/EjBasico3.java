8f95b8b8047ff7bc11e1edaaffc83962524f73eb
jsevilla202 <jsevilla@iesnervion.es>
2022-09-22 13:33:29 +0200

    Comentado EjBasico3

--- EjBasico3/src/ejbasico3/EjBasico3.java
+++ EjBasico3/src/ejbasico3/EjBasico3.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class EjBasico3 {
 
 	public static void main(String[] args) {
-		System.out.println("En que año estamos?");
+		//Pedimos al usuario que inserte el año actual
+		System.out.print("En que año estamos? ");
+		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		//Usamos el scanner para dar valor a la variable "añoa" (año actual)
 		int añoa = sc.nextInt();
-		System.out.println("En que año nacistes?");
-		int añoc = sc.nextInt();
-		int añot = añoa-añoc;
+		//Pedimos al usuario que inserte su año de nacimiento
+		System.out.print("En que año nacistes? ");
+		//Usamos el scanner para dar valor a la variable "añon" (año nacimiento)
+		int añon = sc.nextInt();
+		//Damos valor a la variable "añot" (año total)
+		int añot = añoa-añon;
+		//Mostramos por consola el resultado
 		System.out.println("Tienes " + añot + " años");
 
 	}
