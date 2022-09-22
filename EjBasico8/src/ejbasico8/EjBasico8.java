18a7ac55891337f919cc155d22689751a73abbe2
jsevilla202 <jsevilla@iesnervion.es>
2022-09-22 13:19:02 +0200

    Comentado EjBasico8

--- EjBasico8/src/ejbasico8/EjBasico8.java
+++ EjBasico8/src/ejbasico8/EjBasico8.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class EjBasico8 {
 
 	public static void main(String[] args) {
+		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		//Pedimos al usuario que inserte su nombre
 		System.out.print("Inserte su nombre: ");
+		//Usamos el scanner para dar valor a la variable "nombre"
 		String nombre = sc.nextLine();
+		//Pedimos al usuario que inserte su edad
 		System.out.print("Intruduzca su edad: ");
+		//Usamos el scanner para dar valor a la variable "edad"
 		int edad = sc.nextInt();
+		//Mostramos por consola el resultado
 		System.out.println("Hola " + nombre + ", tienes " + edad + " años, ¡qué mayor eres!");
 	}
 
