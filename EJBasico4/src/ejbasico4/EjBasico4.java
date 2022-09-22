0144c2261e8b7232be28f051ebdbaf6f86440560
jsevilla202 <jsevilla@iesnervion.es>
2022-09-22 13:28:30 +0200

    Comentado EjBasico4

--- EJBasico4/src/ejbasico4/EjBasico4.java
+++ EJBasico4/src/ejbasico4/EjBasico4.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class EjBasico4 {
 
 	public static void main(String[] args) {
-		System.out.println("Cual fue la primera nota?: ");
+		//Pedimos al usuario que inserte su primera nota
+		System.out.print("Cual fue la primera nota?: ");
+		//Creamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		//Usamos el scanner para dar valor a la variable "nota1"
 		float  nota1 = sc.nextFloat();
-		System.out.println("Cual fue la segunda nota?: ");
+		//Pedimos al usuario que inserte su segunda nota
+		System.out.print("Cual fue la segunda nota?: ");
+		//Usamos el scanner para dar valor a la variable "nota2"
 		float nota2 = sc.nextFloat();
+		//Damos valor a la variable "notaf"
 		float notaf = (nota1+nota2)/2;
-		System.out.println("Tu media aritmetica es: " + notaf);
+		//Mostramos por consola el resultado
+		System.out.println("Su media aritmetica es: " + notaf);
 
 	}
 
