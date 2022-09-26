7426ff75fdb91f30eb868d39da42d700c3acaeef
jsevilla202 <jsevilla@iesnervion.es>
2022-09-26 09:33:18 +0200

    Exercises Part 1

---
+++ Exercises1/src/exercise5/Exercise5.java
@@ -0,0 +1,13 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Nombre: Jaime Sevilla Álvarez");
+		System.out.println("Calle: La roda de Andalucía");
+		System.out.println("Número telef. : 686446564");
+		
+
+	}
+
+}
---
+++ Exercises1/src/exercise6/Exercise6.java
@@ -0,0 +1,15 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		System.out.println("Nombre: Jaime Sevilla Álvarez");
+		System.out.println();
+		System.out.println("Calle: La roda de Andalucía");
+		System.out.println();
+		System.out.println("Número telef. : 686446564");
+		
+
+	}
+
+}
